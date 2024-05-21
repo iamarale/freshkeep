44d5528a82c0b63ca28046db169dcb9e0e0779a1
iamarale <ismailarales@gmail.com>
2024-05-21 06:33:43 -0500

    chore: added typesafety to state and now displaying all data from db

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,38 @@
+import { useEffect, useState } from "react";
+import { Supabase } from "./config/supabaseClient";
+interface StockItem {
+  id: bigint;
+  created_at: string;
+  name: string | null;
+  quantity: number | null;
+  expirational_date: string | null;
+  note: string | null;
+  updated_at: string | null;
+}
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [pantryItems, setPantryItems] = useState<StockItem[]>([]);
+
+  useEffect(() => {
+    getStock();
+  }, []);
+
+  async function getStock() {
+    const { data, error } = await Supabase.from("stock").select();
+    setPantryItems(data);
+  }
+
+  return (
+    <main className="grid grid-cols-3 gap-2">
+      {pantryItems &&
+        pantryItems.map((item) => (
+          <div key={item.id} className="border-[1px] p-2 rounded">
+            <h1>{item.name}</h1>
+            <p>{item.note}</p>
+            <div>{item.quantity}</div>
+            <div>{item.updated_at}</div>
+          </div>
+        ))}
+    </main>
+  );
 }
