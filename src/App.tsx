92fab20106ce75080674139af89a257a03b61da9
iamarale <ismailarales@gmail.com>
2024-05-23 10:53:25 -0500

    feat: changed db record name, made homepage responsive and typesafe

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,50 @@
 import { useEffect, useState } from 'react';
 import { Supabase } from './config/supabaseClient';
-interface StockItem {
-  id: bigint;
-  created_at: string;
-  name: string | null;
-  quantity: number | null;
-  expirational_date: string | null;
-  note: string | null;
-  updated_at: string | null;
+import PantryItem from './components/PantryItem';
+interface PantryType {
+  id: number;
+  name: string;
+  category: string;
+  quantity: number;
+  unit: string;
+  expiry_date: Date;
+  date_added: Date;
+  notes: string;
 }
 
 export default function App() {
-  const [pantryItems, setPantryItems] = useState<StockItem[]>([]);
+  const [pantryItems, setPantryItems] = useState<PantryType[]>([]); // Correct type for array of PantryType
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     getStock();
   }, []);
 
   async function getStock() {
-    const { data, error } = await Supabase.from('stock').select();
-    setPantryItems(data);
+    const { data, error } = await Supabase.from('pantry_items').select();
+
+    if (error) {
+      setErrorMessage(`There was an error getting data from supabase ${error}`);
+      setError(true);
+      return;
+    }
+
+    if (data) {
+      setPantryItems(data);
+      setErrorMessage('');
+      setError(false);
+    }
   }
 
   return (
-    <main className="grid grid-cols-3 gap-2 ">
-      {pantryItems &&
-        pantryItems.map(item => (
-          <div key={item.id} className="rounded border-[1px] p-2">
-            <h1>{item.name}</h1>
-            <p>{item.note}</p>
-            <div>{item.quantity}</div>
-            <div>{item.updated_at}</div>
-          </div>
-        ))}
+    <main className="grid grid-cols-1 gap-2 md:grid-cols-3 ">
+      <h1>hell {errorMessage}</h1>
+      {error ? (
+        <h1>{errorMessage}</h1>
+      ) : (
+        <PantryItem pantryItems={pantryItems} />
+      )}
     </main>
   );
 }
