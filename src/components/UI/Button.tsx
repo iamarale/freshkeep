f2abb0187d08fffeca8ab438cbbb46a45a2adaa8
iamarale <ismailarales@gmail.com>
2024-05-23 10:51:20 -0500

    feat: created button comp. and made it typesafe

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,6 +1,36 @@
-export default function Button({ children }: { children: string }) {
+import { ReactNode, ReactElement } from 'react';
+
+type ButtonType = 'success' | 'danger' | 'default';
+
+interface ButtonProps {
+  type: ButtonType;
+  children: ReactNode;
+  icon?: ReactElement;
+}
+
+export default function Button({
+  type = 'default',
+  children,
+  icon,
+}: ButtonProps) {
+  let className;
+
+  switch (type) {
+    case 'success':
+      className = 'border-green-700 border-[1px]';
+      break;
+    case 'danger':
+      className = 'border-red-700 border-[1px]';
+      break;
+    default:
+      className = 'border-neutral-200 border-[1px]';
+  }
+
   return (
-    <button className="rounded bg-neutral-300 px-3 text-neutral-700  duration-200 hover:opacity-80">
+    <button
+      className={`${className} flex  gap-2 rounded px-3 py-1 text-neutral-50 duration-200 hover:opacity-60 md:flex-1 md:justify-between`}
+    >
+      {icon && <span>{icon}</span>}
       {children}
     </button>
   );
